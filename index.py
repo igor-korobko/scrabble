7a86668c7a2410735d7815a93fb84e42f565abcc
user <kis99999@gmail.com>
2014-04-11 15:15:16 +0300

    1

--- index.py
+++ index.py
@@ -1,36 +1,42 @@
+while 1:
+    input_word = raw_input()
 
-input_word = raw_input()
+    points = [["e, a, i, o, n, r, t, l, s, u", 1],
+              ["d, g", 2],
+              ["b, c, m, p", 3],
+              ["f, h, v, w, y", 4],
+              ["k", 5],
+              ["j, x", 8],
+              ["q, z", 10]
+             ]
 
-points = [["e, a, i, o, n, r, t, l, s, u", 1],
-          ["d, g", 2],
-          ["b, c, m, p", 3],
-          ["f, h, v, w, y", 4],
-          ["k", 5],
-          ["j, x", 8],
-          ["q, z", 10]
-         ]
-
-max_count = 0
-t_max_count = 0
-max_word = ""
-flag = True
-
-f1 = open("words.txt", "r")
-for word in f1.readlines():
+    max_count = 0
+    t_max_count = 0
+    max_word = ""
     flag = True
-    for letter in input_word:
-        if letter not in word:
-            flag = False
 
-    if flag and word.count(letter) == input_word.count(letter):
-        for letter in word:
-            for l in points:
-                if letter in l[0]:
-                    t_max_count += l[1]
-        if t_max_count > max_count:
-            max_count = t_max_count
-            max_word = word
-        t_max_count = 0
+    f1 = open("words.txt", "r")
+    for word_from_file in f1.readlines():
+        flag = True
+        for file_letter in word_from_file.strip():
+            if file_letter not in input_word:
+                flag = False
+                break
+            if word_from_file.count(file_letter) > input_word.count(file_letter):
+                flag = False
+                break
 
-f1.close()
-print(max_word)
+        # print(flag)
+        # print(word_from_file)
+        if flag:
+            for letter in word_from_file:
+                for l in points:
+                    if letter in l[0]:
+                        t_max_count += l[1]
+            if t_max_count > max_count:
+                max_count = t_max_count
+                max_word = word_from_file
+            t_max_count = 0
+
+    f1.close()
+    print(max_word)
