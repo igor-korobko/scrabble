4f814cae19019597843e2fe61ec4304a2dc9c67f
user <kis99999@gmail.com>
2014-04-11 14:10:18 +0300

    Initial commit

---
+++ index.py
@@ -0,0 +1,36 @@
+
+input_word = raw_input()
+
+points = [["e, a, i, o, n, r, t, l, s, u", 1],
+          ["d, g", 2],
+          ["b, c, m, p", 3],
+          ["f, h, v, w, y", 4],
+          ["k", 5],
+          ["j, x", 8],
+          ["q, z", 10]
+        ]
+
+max_count = 0
+t_max_count = 0
+max_word = ""
+flag = True
+
+f1 = open("words.txt", "r")
+for word in f1.readlines():
+    flag = True
+    for letter in input_word:
+        if letter not in word:
+            flag = False
+
+    if flag:
+        for letter in word:
+            for l in points:
+                if letter in l[0]:
+                    t_max_count += 1
+        if t_max_count > max_count:
+            max_count = t_max_count
+            max_word = word
+        t_max_count = 0
+
+f1.close()
+print(max_word)
